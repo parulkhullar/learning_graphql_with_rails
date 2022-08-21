ba3ec7f2e5f41a339950f168426c0a733a7d1e0a
parulkhullar <parul734@gmail.com>
2022-08-20 18:14:23 -0700

    initial commit

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :email, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :articles, [Types::ArticleType], null: false
+    field :article_count, Integer, null: true
+
+    def article_count
+      object.articles.size
+    end
+  end
+end
